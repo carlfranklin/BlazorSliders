0926eeb736c973213d9e2678fc6dd3e3ba1acb36
Simon Cropp <simon.cropp@gmail.com>
2021-03-20 12:09:35 +1100

    add snapshot testing

---
+++ UnitTests/ModuleInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using ImageMagick;
+using Verify.AngleSharp;
+using VerifyTests;
+
+public static class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        // remove some noise from the html snapshot
+
+        VerifierSettings.ScrubEmptyLines();
+        VerifierSettings.ScrubLinesWithReplace(s => s.Replace("<!--!-->", ""));
+        HtmlPrettyPrint.All();
+        VerifierSettings.ScrubLinesContaining("<script src=\"_framework/dotnet.");
+
+        VerifyPlaywright.Enable();
+        VerifyImageMagick.RegisterComparers(
+            threshold: .01,
+            metric: ErrorMetric.MeanAbsolute);
+    }
+}
---
+++ UnitTests/PlaywrightFixture.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using PlaywrightSharp;
+using PlaywrightSharp.Chromium;
+using Xunit;
+
+public class PlaywrightFixture :
+    IAsyncLifetime
+{
+    IPlaywright? playwright;
+    Process? process;
+    IChromiumBrowser? browser;
+
+    public async Task InitializeAsync()
+    {
+        StartBlazorApp();
+        playwright = await Playwright.CreateAsync();
+
+        browser = await playwright.Chromium.LaunchAsync();
+
+        Page = await browser.NewPageAsync();
+        Page.ViewportSize.Height = 768;
+        Page.ViewportSize.Width = 1024;
+    }
+
+    public IPage Page { get; private set; } = null!;
+
+    void StartBlazorApp()
+    {
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory!;
+        var binPath = baseDirectory.Replace("UnitTests", "BlazorSliderTestWasm");
+        var projectDir = Path.GetFullPath(Path.Combine(binPath, "../../../"));
+        ProcessStartInfo startInfo = new("dotnet", "run")
+        {
+            WorkingDirectory = projectDir
+        };
+        process = Process.Start(startInfo);
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (browser != null)
+        {
+            await browser.DisposeAsync();
+        }
+
+        playwright?.Dispose();
+
+        if (process != null)
+        {
+            process.Kill();
+            process.Dispose();
+        }
+    }
+}
